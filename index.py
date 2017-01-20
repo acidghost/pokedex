d4a89242b5f27f8b3d69d934e65e43b1cf3308ef
Andrea Jemmett <andreajemmett@gmail.com>
2017-01-20 15:16:34 +0100

    compute SIFT descriptors and store them

--- index.py
+++ index.py
@@ -3,11 +3,13 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 import time, cPickle, argparse
+import cv2
 
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-p', '--pokemon-list', action='store_true', help='Download the pokemon list')
 ap.add_argument('-s', '--sprites', action='store_true', help='Download sprites too')
+ap.add_argument('-i', '--index', action='store_true', help='Index sprites')
 args = ap.parse_args()
 
 
@@ -15,12 +17,20 @@ if args.sprites or args.pokemon_list:
     # get pokemons list from pokemondb
     pdb_page = requests.get('http://pokemondb.net/pokedex/national')
     pdb_soup = BeautifulSoup(pdb_page.text)
-    pdb = [{
-        'number': int(elm.find('small').contents[0].split('#')[1]),
-        'slug':   elm.find('a', { 'class': 'ent-name' }).get('href').split('pokedex/')[1],
-        'name':   bytearray(elm.find('a', { 'class': 'ent-name' }).contents[0], encoding='utf-8')
-    } for elm in pdb_soup.findAll('span', { 'class': 'infocard-tall ' })]
-    #                                        this is intended -----^
+    pdb = []
+    for elm in pdb_soup.findAll('span', { 'class': 'infocard-tall ' }):
+        #                                  this is intended -----^
+        aelm = elm.find('a', { 'class': 'ent-name' })
+        pnumber = int(elm.find('small').contents[0].split('#')[1])
+        pslug = aelm.get('href').split('pokedex/')[1]
+        pname = bytearray(aelm.contents[0], encoding='utf-8')
+
+        pdb.append({
+            'number': pnumber,
+            'slug':   pslug,
+            'name':   pname,
+            'sprite': '{:03d}.{}.png'.format(pnumber, pslug)
+        })
 
     # dump db into file
     with open('./pdb.pkl', 'wb') as f:
@@ -58,3 +68,24 @@ if args.sprites:
 
         # be polite...
         time.sleep(1)
+
+
+if args.index:
+    # load db if not loaded
+    if not args.pokemon_list:
+        with open('./pdb.pkl', 'rb') as f:
+            pdb = cPickle.load(f)
+    sift = cv2.SIFT()
+    total_kps = 0
+    for pokemon in pdb:
+        # load sprite and convert to gray scale
+        img = cv2.imread('./sprites/' + pokemon['sprite'])
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # compute SIFT descriptors
+        kp, pokemon['descriptors'] = sift.detectAndCompute(gray, None)
+        total_kps += len(kp)
+
+    # store DB with descriptors
+    with open('./pdb.pkl', 'wb') as f:
+        cPickle.dump(pdb, f)
+    print '[INDEX] Total KPs: {}'.format(total_kps)
